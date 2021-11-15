bfef0c8b689b0ea149be8987d5ee3da1acb134bc
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2021-11-15 15:23:00 -0500

    first commit

---
+++ src/main/java/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+
+    /*
+     * Complete the 'alternatingCharacters' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public int alternatingCharacters(String s) {
+        // Write your code here
+        return 0;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp(){
+        solution = new Solution();
+    }
+
+    @Test
+    public void alternatingCharactersTest01(){
+        String input = "AAAA";
+        int expected = 3;
+        int actual = solution.alternatingCharacters(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void alternatingCharactersTest02(){
+        String input = "BBBBB";
+        int expected = 4;
+        int actual = solution.alternatingCharacters(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void alternatingCharactersTest03(){
+        String input = "ABABABAB";
+        int expected = 0;
+        int actual = solution.alternatingCharacters(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void alternatingCharactersTest04(){
+        String input = "BABABA";
+        int expected = 0;
+        int actual = solution.alternatingCharacters(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void alternatingCharactersTest05(){
+        String input = "AAABBB";
+        int expected = 4;
+        int actual = solution.alternatingCharacters(input);
+        Assertions.assertEquals(expected, actual);
+    }
+}
